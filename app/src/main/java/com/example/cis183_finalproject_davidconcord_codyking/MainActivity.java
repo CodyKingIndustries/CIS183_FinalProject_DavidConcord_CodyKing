86b42038d82a5fc93b32383e881b2fd425c968c7
CodyKingIndustries <128918453+CodyKingIndustries@users.noreply.github.com>
2023-11-04 11:18:34 -0400

    Making Repository
    Starting the project

---
+++ app/src/main/java/com/example/cis183_finalproject_davidconcord_codyking/MainActivity.java
@@ -0,0 +1,22 @@
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//Authors: Cody King
+//Date: 11/4/2023
+//Description: This is a scheduling App
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+package com.example.cis183_finalproject_davidconcord_codyking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
